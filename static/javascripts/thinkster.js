13472c97c7510e6d01d1e383958a708f25948c77
Meet Shah <meet1995@gmail.com>
2015-03-28 18:38:32 +0000

    first commit

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -1,2 +1,26 @@
-angular
-  .module('thinkster', []);
+(function () {
+  'use strict';
+
+  angular
+    .module('thinkster', [
+      'thinkster.routes',
+      'thinkster.authentication','thinkster.config'
+    ]);
+
+  angular
+    .module('thinkster.routes', ['ngRoute']);
+  
+  angular
+    .module('thinkster.config',[]);
+  
+  angular
+    .module('thinkster')
+    .run(run);
+  
+  run.$inject = ['$http'];
+  
+  function run($http){
+    $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+    $http.defaults.xsrfCookieName = 'csrftoken';
+  }
+})();
--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,9 +1,18 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 
 from thinkster_django_angular_boilerplate.views import IndexView
 
+from rest_framework import routers
+
+from authentication.views import AccountViewSet
+
+router = routers.SimpleRouter()
+router.register(r'accounts',AccountViewSet)
+
+
 urlpatterns = patterns(
     '',
-
+    url(r'^api/v1/',include(router.urls)),
+  url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url('^.*$', IndexView.as_view(), name='index'),
 )
